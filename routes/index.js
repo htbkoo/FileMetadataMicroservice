f0cab72db53c774c6d315b7927c9addcdee4c41d
H.T. Koo <h1017760@connect.hku.hk>
2017-07-07 08:59:12 +0800

    Cleaned up and removed unused FreeCodeCamp -  API Backend project - File Metadata Microservice

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 import express from 'express';
 let router = express.Router();
 
-import serverHostNameFormatter from "../service/serverHostNameFormatter";
-
-function getFullHostNameFromReq(req) {
-    return serverHostNameFormatter.appendProtocolToHostName(req.headers.host);
-}
-
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('index', {
-        "serverHostNameWithProtocol": getFullHostNameFromReq(req)
-    });
+    res.render('index');
 });
 
 router.post('/size', (req, res) => {
