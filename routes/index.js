49df1196de864d9ab6d27b9230195ea1fae00d80
H.T. Koo <h1017760@connect.hku.hk>
2017-07-07 08:31:59 +0800

    Updated index.js FreeCodeCamp -  API Backend project - File Metadata Microservice

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,17 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+let router = express.Router();
 
-var urlShortenerMicroservice = require("../service/urlShortenerMicroservice");
-var serverHostNameFormatter = require("../service/serverHostNameFormatter");
+import serverHostNameFormatter from "../service/serverHostNameFormatter";
 
-/* GET home page. */
 function getFullHostNameFromReq(req) {
     return serverHostNameFormatter.appendProtocolToHostName(req.headers.host);
 }
 
-router.get('/', function (req, res) {
+/* GET home page. */
+router.get('/', (req, res) => {
     res.render('index', {
         "serverHostNameWithProtocol": getFullHostNameFromReq(req)
     });
-});
-
-router.get('/shorten/*', function (req, res) {
-    var url = req.params['0'];
-    urlShortenerMicroservice.tryShortening(url, getFullHostNameFromReq(req)).then(function (jsonResponse) {
-            res.send(jsonResponse);
-        }
-    );
-});
-
-router.get(/\/(.+)/, function (req, res) {
-    var urlParam = req.params['0'];
-    urlShortenerMicroservice.searchForOriginalUrl(urlParam, getFullHostNameFromReq(req)).then(function (jsonResponse) {
-            if ('shorten_from' in jsonResponse) {
-                res.redirect(jsonResponse['shorten_from']);
-            } else {
-                res.send(jsonResponse);
-            }
-        }
-    );
 });
 
 module.exports = router;
