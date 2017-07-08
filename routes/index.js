86be6c12833a865dc5c02621cbc7a1a4bc3e68a1
H.T. Koo <h1017760@connect.hku.hk>
2017-07-08 14:15:52 +0800

    No permission to create temp folder on Glitch FreeCodeCamp -  API Backend project - File Metadata Microservice

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import multer  from 'multer';
 import fileSizeFinder from '../service/fileSizeFinder';
 
 let router = express.Router();
-let upload = multer({dest: '/temp/'});
+let upload = multer({dest: '/'});
 
 /* GET home page. */
 router.get('/', (req, res) => {
