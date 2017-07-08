e55d1b4036b157596eb7f3e89ff753ca648e1789
H.T. Koo <h1017760@connect.hku.hk>
2017-07-08 13:55:48 +0800

    Updated export to ES6 export sytle FreeCodeCamp -  API Backend project - File Metadata Microservice

--- service/fileSizeFinder.js
+++ service/fileSizeFinder.js
@@ -2,9 +2,9 @@
  * Created by Hey on 7 Jul 2017
  */
 
-module.exports = {
+export default {
     "sizeOf": function (file) {
-        if (typeof file === "undefined"){
+        if (typeof file === "undefined") {
             return "No attachment found!";
         }
         return file.size;
